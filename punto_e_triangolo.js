cff543dd147568a7dae06daf5a2a6e6164dcb31c
Andrea Somma <andreasomma89@gmail.com>
2012-03-09 18:10:55 +0100

    aggiunto semipiano

--- punto_e_triangolo.js
+++ punto_e_triangolo.js
@@ -1,25 +1,77 @@
-function Point(x,y) {
-	this.x = x;
-	this.y = y;
-}
-Point.prototype.distance = function(p) {
-	var x_distance = this.x - p.x;
-	var y_distance = this.y - p.y;
-	return Math.sqrt(Math.pow(x_distance,2)
-		+Math.pow(y_distance,2));
+/* randomPoints */
+
+var sqrt = Math.sqrt;
+var pow = Math.pow;
+var random = Math.random;
+
+/* POINT */
+
+var Point = function (x, y) {
+  this.x = x || 0;
+  this.y = y || 0;
 }
 
-function Triangle(p1, p2, p3) {
-	var l1 = p1.distance(p2);
-	var l2 = p1.distance(p3);
-	var l3 = p2.distance(p3);
-	this.perimeter = function() {
-		return l1 + l2 + l3;
-	}
-	this.area = function() {
-		var semiperimeter = this.perimeter()/2;
-		return Math.sqrt(semiperimeter*(semiperimeter-l1)*(semiperimeter-l2)*(semiperimeter-l3));
-	}
+Point.prototype.getDistance = function(point) {
+  return sqrt(pow(point.y - this.y, 2) + pow(point.x - this.y, 2));
+};
+
+Point.prototype.translate = function(dx, dy) {
+  this.x += dx;
+  this.y += dy;
+
+  return this;
+};
+
+
+/* TRIANGLE */
+
+var Triangle = function (p1, p2, p3) {
+  this.p1 = p1;
+  this.p2 = p2;
+  this.p3 = p3;
+
+  this.l1 = p2.getDistance(p3);
+  this.l2 = p3.getDistance(p1);
+  this.l3 = p1.getDistance(p2);
 }
-Triangle.prototype.getArea = function() {return this.area();}
-Triangle.prototype.getPerimeter = function() {return this.perimeter();}
+
+Triangle.prototype.getPerimeter = function() {
+  return this.l1 + this.l2 + this.l3;
+};
+
+Triangle.prototype.getArea = function() {
+  var p = this.getPerimeter() / 2;
+
+  return sqrt(p*(p - this.l1)*(p - this.l2)*(p - this.l3));
+};
+
+
+var randomPoint = function () {
+  var x1 = random() * 200 - 100;
+  var y1 = random() * 200 - 100;
+  
+  return new Point(x1, y1);
+};
+
+var randomPoints = function (n) {
+  var n = n || 1;
+  var res = new Array(n);
+
+  for (var i = 0; i < n ; i += 1) {
+    res[i] = randomPoint();
+  }
+
+  return res;
+}
+
+var points = randomPoints(100);
+
+function SemipianoPositivo(point) {
+	return point.x < point.y;
+}
+
+function FiltraSemipianoPositivo(array) {
+	return array.filter(function(point) {
+		return SemipianoPositivo(point);
+	});
+}
